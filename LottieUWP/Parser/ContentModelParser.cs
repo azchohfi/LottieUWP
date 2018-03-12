196f2059bacc5a8e5627c8a4dc51772307497517
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2018-03-12 12:08:16 -0700

    Added a warning for merge paths

--- LottieUWP/Parser/ContentModelParser.cs
+++ LottieUWP/Parser/ContentModelParser.cs
@@ -66,6 +66,9 @@ namespace LottieUWP.Parser
                     break;
                 case "mm":
                     model = MergePathsParser.Parse(reader);
+                    composition.AddWarning("Animation contains merge paths. Merge paths are only " +
+                        "supported on KitKat+ and must be manually enabled by calling " +
+                        "enableMergePathsForKitKatAndAbove().");
                     break;
                 case "rp":
                     model = RepeaterParser.Parse(reader, composition);
